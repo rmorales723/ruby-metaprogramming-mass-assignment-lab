4a37fb19d79f331fb302ca9d7bf0b097dd21bbd2
robert morales <rmoralfit@gmail.com>
2020-11-19 15:39:53 -0500

    commit tree 6b911eb64444fa12d6589a2f73b6d0ab523307c1

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
 class Person
-  #your code here
-end
+    def initialize(attributes)
+      attributes.each do |key, value| 
+        self.class.attr_accessor(key)
+        self.send(("#{key}="), value)
+      end
+    end
+  end 
